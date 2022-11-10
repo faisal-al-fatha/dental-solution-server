bd860788033e84ae282ffa3a50f582971544ed3c
faisal-al-fatha <faysal313@gmail.com>
2022-11-10 12:02:03 +0600

    review api created

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -13,14 +13,38 @@ console.log(process.env.DB_DENTAL);
 console.log(process.env.DB_PASSWORD);
 
 
-const uri = 'mongodb://localhost:27017';
-// const uri = `mongodb+srv://${process.env.DB_DENTAL}:${process.env.DB_PASSWORD}@cluster0.eveocjd.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = 'mongodb://localhost:27017';
+const uri = `mongodb+srv://${process.env.DB_DENTAL}:${process.env.DB_PASSWORD}@cluster0.eveocjd.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
 try {
     const serviceCollection = client.db('dentalSolution').collection('services');
+    const reviewCollection = client.db('dentalSolution').collection('reviews');
+
+    app.get('/services', async(req,res)=>{
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+    });
+
+    app.get('/services/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const service = await serviceCollection.findOne(query);
+        res.send(service);
+    });
+
+    // reviews apis  here 
+    app.post('/reviews', async (req, res) => {
+        const review = req.body;
+        const result = await  reviewCollection.insertOne(review);
+        res.send(result);
+    });
+
+
 } 
 finally {
     
